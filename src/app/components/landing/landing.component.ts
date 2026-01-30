6ce80c28b667cc28b139de83f3503b046b6297ae
Preston Lee <preston.lee@prestonlee.com>
2026-01-30 12:02:59 -0700

    Landing page URL summary

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,15 +1,20 @@
 // Author: Preston Lee
 
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, computed } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterLink],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
+  readonly fhirBaseUrl = computed(() => this.settingsService.getEffectiveFhirBaseUrl());
+  readonly terminologyBaseUrl = computed(() => this.settingsService.getEffectiveTerminologyBaseUrl());
+
+  constructor(private readonly settingsService: SettingsService) {}
 }
 
